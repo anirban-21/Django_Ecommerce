e562937d727fbd1fde605e1cdb63874f95845276
Anirban Raha <anirbanraha08@gmail.com>
2021-07-04 10:22:51 +0530

    checkout page

--- cart/urls.py
+++ cart/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 	path('add-cart/<int:product_id>/', views.add_cart, name='add-cart'),
 	path('remove-cart/<int:product_id>/', views.remove_cart, name='remove_cart'),
 	path('remove-cart_item/<int:product_id>/', views.remove_cart_item, name='remove_cart_item'),
+	path('checkout/', views.checkout, name='checkout'),
 ]
